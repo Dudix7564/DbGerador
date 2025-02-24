9cba6e36b3d85da078eeb8bce76e09b20d5dbc62
matheusviniciusva <matheusviniciusva@gmail.com>
2025-02-24 09:24:23 -0300

    atualização mídia.js e mídia.ejs

--- routes/midia.js
+++ routes/midia.js
@@ -5,11 +5,7 @@ let db = require('../utils/db')
 
 // /* Página de cadastro */
 router.get('/midia', function(req, res) {
-    res.render('midia', { title: 'Mídia' });
-});
-
-router.get('/midia', function(req, res) {
-    db.query('SELECT (NoMídia, AnoMídia) FROM tbmídia', [], function(erro, listagem) {
+    db.query('SELECT NoMídia, AnoMídia FROM tbmídia', [], function(erro, listagem) {
         if (erro) {
             console.error(erro);
             return res.send('Erro ao listar mídia.');
