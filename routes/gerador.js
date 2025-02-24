a56be823e14038df91257b1671f3394ff725a1b5
matheusviniciusva <matheusviniciusva@gmail.com>
2025-02-24 10:15:34 -0300

    atualizaçãa midiavaliada

--- routes/gerador.js
+++ routes/gerador.js
@@ -2,15 +2,13 @@ let express = require('express');
 let router = express.Router();
 let db = require('../utils/db');
 
-router.get('/gerador', function(req, res) {
-    db.query('SELECT * FROM tbmídia', [], function(erro, listagem){
-        if (erro){
-        res.send(erro);
+router.get('/gerador', function (req, res) {
+    db.query('SELECT * FROM tbmídia', [], function (erro, listagem) {
+        if (erro) {
+            res.send(erro);
         }
         res.send(listagem);
-        res.render('mídia', {resultado: listagem});
-        });
+        res.render('mídia', { resultado: listagem });
     });
- 
-    
-    module.exports = router;
+});
+module.exports = router;
---
+++ routes/midiaavaliada.js
@@ -0,0 +1,16 @@
+let express = require('express');
+let router = express.Router();
+let db = require('../utils/db');
+
+router.get('/midiaavaliada', function (req, res) {
+    db.query('SELECT * FROM `tbusuáriomidia`', [], function (erro, resultado) {
+        if (erro) {
+            res.send(erro);
+            return res.send('Erro ao processar.');
+        }
+        res.send(resultado);
+    });
+});
+
+
+module.exports = router;
