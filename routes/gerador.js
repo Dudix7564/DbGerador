92ca5d13b0a54bbff42b3a17eeb96abaed10c962
Eduarda <duda891098@gmail.com>
2025-03-01 16:20:13 -0300

    tentando criar página de usuário

--- routes/gerador.js
+++ routes/gerador.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 let db = require('../utils/db');
 
-router.get('/gerador', function (req, res) {
+router.get('/', function (req, res) {
     db.query('SELECT * FROM tbmídia', [], function (erro, listagem) {
         if (erro) {
             res.send(erro);
