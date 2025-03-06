f2697cd9a45c8072c134dd87b7a7fe3ac5907072
matheusviniciusva <matheusviniciusva@gmail.com>
2025-03-06 14:49:54 -0300

    atualização gerador.js

--- routes/gerador.js
+++ routes/gerador.js
@@ -4,7 +4,7 @@ let db = require('../utils/db');
 
 router.get('/:matricula', function (req, res) {
     let matricula = req.params.matricula
-    db.query('SELECT * FROM tbmídia', [], function (erro, listagem) {
+    db.query('SELECT m.CodMídia, m.NoMídia, tm.NoTipoMídia AS TipoMídia, gm.NoGênero AS GêneroMídia, sm.NoStreaming AS Streaming, m.AnoMídia FROM tbmídia m JOIN tbtipomídia tm ON m.TipoMídia = tm.TipoMídia JOIN tbgêneros gm ON m.GêneroMídia = gm.GêneroMídia JOIN tbstreaming sm ON m.Streaming = sm.Streaming ORDER BY m.CodMídia', [], function (erro, listagem) {
         if (erro) {
             res.send(erro);
         }
